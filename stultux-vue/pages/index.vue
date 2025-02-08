48b3afe86451be08634a47a39a7dacc8bc3575d9
Sebastian <sebos@protonmail.com>
2025-02-08 13:24:56 +0100

    fix: remove unused files

--- stultux-vue/pages/index.vue
+++ stultux-vue/pages/index.vue
@@ -7,50 +7,53 @@ const login = ref('dupa')
 const password = ref('dupa')
 
 definePageMeta({
-    layout: 'centered-layout',
+  layout: 'centered-layout',
 })
 </script>
 
 <template>
-    <div class="bg-[#f2ebdc] dark:bg-zinc-950 dark:text-white">
-        <BaseCard color="red" class="py-40">
-            <div
-                class="mx-auto flex max-w-[300px] flex-col justify-center gap-4"
-            >
-                <div>
-                    <BaseInput
-                        v-model="login"
-                        label="Email"
-                        name="email"
-                        placeholder="Email"
-                    />
-                </div>
+  <div class="bg-[#f2ebdc] dark:bg-zinc-950 dark:text-white">
+    <BaseCard
+      color="red"
+      class="py-40"
+    >
+      <div
+        class="mx-auto flex max-w-[300px] flex-col justify-center gap-4"
+      >
+        <div>
+          <BaseInput
+            v-model="login"
+            label="Email"
+            name="email"
+            placeholder="Email"
+          />
+        </div>
 
-                <div>
-                    <BaseInput
-                        v-model="password"
-                        label="Password"
-                        name="password"
-                        placeholder="Password"
-                    />
-                </div>
+        <div>
+          <BaseInput
+            v-model="password"
+            label="Password"
+            name="password"
+            placeholder="Password"
+          />
+        </div>
 
-                <div>
-                    <BaseButton
-                        class="mb-4 w-full"
-                        @click="$colorMode.preference = 'dark'"
-                    >
-                        Login
-                    </BaseButton>
+        <div>
+          <BaseButton
+            class="mb-4 w-full"
+            @click="$colorMode.preference = 'dark'"
+          >
+            Login
+          </BaseButton>
 
-                    <BaseButton
-                        class="w-full"
-                        @click="$colorMode.preference = 'light'"
-                    >
-                        Create Account
-                    </BaseButton>
-                </div>
-            </div>
-        </BaseCard>
-    </div>
+          <BaseButton
+            class="w-full"
+            @click="$colorMode.preference = 'light'"
+          >
+            Create Account
+          </BaseButton>
+        </div>
+      </div>
+    </BaseCard>
+  </div>
 </template>
